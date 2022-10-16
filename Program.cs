04c2bbc8a6d7c2fbaf5b33c49aa658d979f016e6
Vera <imbesom@mail.ru>
2022-10-17 01:32:45 +0300

    финальное дз

---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿void Zadacha64()
+{
+    // Задайте значения M и N. Напишите рекурсивный 
+    // метод, который выведет 
+    // все натуральные числа кратные 3-ём в промежутке от M до N.
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+int temp = m;
+
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+
+PrintTri(m, n);
+
+void PrintTri(int m, int n)
+{
+    while (m<n)
+    {
+        if (m%3==0)
+        {
+           System.Console.WriteLine(m); 
+        }
+        m=m+1;
+    }
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+}
+
+void Zadacha66()
+{
+    // Задача 66: Задайте значения M и N.
+    //  Напишите программу, которая найдёт сумму
+    //  натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+int temp = m;
+
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+
+PrintSumm(m, n, temp=0);
+
+void PrintSumm(int m, int n, int summ)
+{
+  summ = summ + n;
+  if (n <= m)
+  {
+    Console.Write($"Сумма элементов= {summ} ");
+    return;
+  }
+  PrintSumm(m, n - 1, summ);
+}
+
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+}
+void Zadacha68()
+{
+    // Задача 68: Напишите программу вычисления 
+    // функции Аккермана с помощью рекурсии. Даны два
+    //  неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+Console.Clear();
+Console.WriteLine($"Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.");
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+
+int functionAkkerman = Ack(m, n);
+
+Console.Write($"Функция Аккермана = {functionAkkerman} ");
+
+int Ack(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack(m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+}
+
+// Zadacha64();
+// Zadacha66()
+// Zadacha68();
